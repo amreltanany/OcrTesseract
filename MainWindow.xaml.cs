58c36133dcc2e03a8963ada0654976ca72ae68c2
Amr Eltanany <148919790+amreltanany@users.noreply.github.com>
2025-09-18 17:12:21 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using OCRApp.Common.Exteneions;
+using OCRApp.Common.Types;
+using System.Windows;
+
+namespace OCRApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void OpenImage(object sender, RoutedEventArgs e) =>
+            new OpenFileDialog()
+            .ShowImageDialog()
+            .Do(StartProcess);
+
+        private void StartProcess(ImageFilePath imageFilePath) =>Image.FilePath = Ocr.filePath = imageFilePath;
+
+        private void ImageLoaded(object sender, EventArgs e) => Ocr.OcrImage();
+        private void Image_ImageLoaded(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
